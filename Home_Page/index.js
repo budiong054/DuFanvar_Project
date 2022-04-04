4be440cf8cc3c374fd54ba7c17a938ff2d6ec1be
Blessing Godwin Udiong <82527332+budiong054@users.noreply.github.com>
2022-04-04 16:40:33 +0100

    updated the link and the hambuger icon for the contact page

--- Home_Page/index.js
+++ Home_Page/index.js
@@ -7,17 +7,12 @@
 
     const displayOpen = ()=> {
         mobileNavContainer.style.marginLeft = '0%';
-        // body.style.backgroundColor = 'rgba(0,0,0)';
-        // header.style.color = 'black'
-        // header.style.background = 'white'
     } 
 
     menu.addEventListener('click', displayOpen)
 
     const displayClose = () => {
     mobileNavContainer.style.marginLeft = '-300%';
-    // mobileNavContainer.style.transition = '1s'
-    // header.style.backgroundImage = 'url(https://github.com/budiong054/DuFanvar_Project/blob/main/Images/green_vegetation.png?raw=true)';
     }
 
     close.addEventListener('click', displayClose)
